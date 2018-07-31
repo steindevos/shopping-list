39680deae32b3255288ec6c169d3e1e250a75a2b
SDV <steindevos@gmail.com>
2018-07-31 14:24:37 +0000

    added the 'add task' section

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -16,7 +16,18 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
-    
+@app.route("/addtask")
+def add_task():
+    return render_template("addtask.html",
+    categories=mongo.db.categories.find())
+
+@app.route("/insert_task", methods=["POST"])
+def insert_task(): 
+    tasks=mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for("get_tasks"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
