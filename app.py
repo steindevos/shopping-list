f142da37e6762c6b01399cb96e5e08f8e228d461
SDV <steindevos@gmail.com>
2018-07-31 09:56:59 +0000

    connected to the database

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "ci-project"
+app.config["MONGO_URI"] = "mongodb://Admin:chocoladet55rt@ds161411.mlab.com:61411/ci-project"
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def show_index():
-    return "hello world"
+@app.route("/get_tasks")
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
